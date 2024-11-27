fa422ba9cb49a3f7486f2ab10fbc1d100fbc446b
Thiago Riciopo <thiago.riciopo@fatec.sp.gov.br>
2024-11-26 21:10:39 -0300

    corrige função soma

--- app/src/main/java/com/example/bredufateczlsoex02/controller/NumeroController.java
+++ app/src/main/java/com/example/bredufateczlsoex02/controller/NumeroController.java
@@ -2,7 +2,12 @@ package com.example.bredufateczlsoex02.controller;
 
 public class NumeroController {
     public String soma(int a, int b) {
-        return String.valueOf(a + b);
+        int sum = 0;
+        for (int i = a+1; i < b; i++) {
+            if (i % 2 != 0) sum += i;
+        }
+
+        return String.valueOf(sum);
     }
 
     public String maior(int a, int b) {
